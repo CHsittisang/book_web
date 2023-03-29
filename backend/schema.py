bebf594a707c9c72ee43c75ff49df3129595d0c6
nupassion201035 <fapfap201035@gmail.com>
2023-03-29 21:25:49 +0700

    first commit

---
+++ backend/schema.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class AccountSchema(BaseModel):
+    id : str
+    password: str
+    email: str
+    name: str
+    phone: str
+
+
+class BookSchema(BaseModel):
+    book_name: str 
+    type: str
+    tag: str
+    price: int
+    releae_date: str
+    suply: str
+    author: str
+
+    
+class SeriesSchema(BaseModel):
+    last_Update: str
+    series_name: str
+    imagre: str
+    
+    
+
+    
