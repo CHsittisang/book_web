2bbfa192f31af29bd6474103ca358a3ac1950d86
nupassion201035 <fapfap201035@gmail.com>
2023-03-30 22:09:23 +0700

    New form

--- backend/system.py
+++ backend/system.py
@@ -31,8 +31,6 @@ admin1 = admin(id="admin", password="admin", name="admin", email="admin", phone=
 admin2 = admin(id="admin2", password="admin2", name="admin2", email="admin2", phone="admin2", permission="admin2")
 
 
-book1 = Book(book_name="book1", book_id=1, author="author1", detail_in_book="detail1", type="type1", tag="tag1", price=100, img="img1", releae_date="releae_date1", number_of_product=1)
-
 book1 = Book(book_name="book1", book_id=1, author="author1", detail_in_book="detail1", type="type1", tag="tag1", price=1000, img="img1", releae_date="date1", number_of_product=1)
 book2 = Book(book_name="book2", book_id=2, author="author2", detail_in_book="detail2", type="type2", tag="tag2", price=2000, img="img2", releae_date="date2", number_of_product=2)
 catalog1= Book_catalog(catalog_name="catalog1", detail_series="detail1", type="type1", tag="tag1", img="img1", releae_date="date1", catalog_book_list=[])
