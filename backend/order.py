a6acadf5518dbc21efd5b1dcb50a52378f4cd38a
Thongtae sirimak <65015057@kmitl.ac.th>
2023-04-12 22:47:00 +0700

    change dataclass to normal form

--- backend/order.py
+++ backend/order.py
@@ -2,12 +2,12 @@ from account import Customer
 class order(Customer):
     def __init__(self,id, password, status, name, email, phone,address,order_id, total_price, order_status, order_date, order_in_cart, shipment_detal):
         super().__init__(order_id, total_price, order_status, order_date, order_in_cart, shipment_detal)
-    order_id: int
-    total_price: int
-    order_status: str
-    order_date: str
-    order_in_cart: str
-    shipment_detal: str
+        self.order_id = order_id
+        self.total_price = total_price
+        self.order_status = order_status
+        self.order_date = order_date
+        self.order_in_cart = order_in_cart
+        self.shipment_detal = shipment_detal
 
 class order_history(order):
     def __init__(self,order_id, total_price, order_status, order_date, order_in_cart, shipment_detal):
